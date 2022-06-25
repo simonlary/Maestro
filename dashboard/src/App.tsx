decca57239a3849c9dd5b0c96af8493b305f15a2
Randy Sab-Roy <randy.sab.roy@gmail.com>
2022-06-25 19:16:38 -0400

    Add TailwindCSS

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
-
-export default App;
