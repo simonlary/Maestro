b11deb6c082e2ccb5c44bcb7f591c426a6ff65f1
simonlary <simonlary@gmail.com>
2022-07-10 00:50:34 -0400

    Use icon in app bar.

--- dashboard/src/components/AppBar.tsx
+++ dashboard/src/components/AppBar.tsx
@@ -1,24 +1,24 @@
 import { NavLink } from "react-router-dom";
+import headerIcon from "./header-icon.svg";
 
 export function AppBar() {
   const getNavClass = ({ isActive }: { isActive: boolean }) =>
     `flex items-center p-2 h-full hover:bg-gray-2 font-bold ${isActive ? " border-b " : "text-soft-white"}`;
 
   return (
-    <nav className="bg-gray-1 text-white">
-      <div className="mx-auto px-8">
-        <div className="relative flex items-center h-10">
-          <h1 className="font-bold text-gray-text pr-4 select-none">Maestro</h1>
-          <NavLink to="/guilds" className={getNavClass}>
-            Guilds
-          </NavLink>
-          <NavLink to="/logs" className={getNavClass}>
-            Logs
-          </NavLink>
-          <NavLink to="/settings" className={getNavClass}>
-            Settings
-          </NavLink>
-        </div>
+    <nav className="flex items-center h-10 bg-gray-1 text-white">
+      <img className="h-full p-1" src={headerIcon} />
+      <h1 className="font-bold text-white -ml-2 mr-4 select-none">aestro</h1>
+      <div className="relative flex items-center h-10">
+        <NavLink to="/guilds" className={getNavClass}>
+          Guilds
+        </NavLink>
+        <NavLink to="/logs" className={getNavClass}>
+          Logs
+        </NavLink>
+        <NavLink to="/settings" className={getNavClass}>
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
