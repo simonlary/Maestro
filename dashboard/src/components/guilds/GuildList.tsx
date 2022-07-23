428f4e577b7bc0a46dc2bcf0913c82c3cef8a745
simonlary <simonlary@gmail.com>
2022-07-23 19:30:31 -0400

    Sort the guilds alphabetically and put those with something currently playing at the top.

--- dashboard/src/components/guilds/GuildList.tsx
+++ dashboard/src/components/guilds/GuildList.tsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 interface GuildInfo {
   id: string;
   name: string;
-  icon?: string | null;
+  icon?: string;
+  isPlaying: boolean;
 }
 
 function GuildEntry({ guild }: { guild: GuildInfo }) {
@@ -26,10 +27,16 @@ function GuildEntry({ guild }: { guild: GuildInfo }) {
 }
 
 export function GuildList({ guilds }: { guilds: GuildInfo[] }) {
+  const sortedGuilds = guilds.sort((a, b) => a.name.localeCompare(b.name));
+  const playingGuilds = sortedGuilds.filter((guild) => guild.isPlaying);
+  const notPlayingGuilds = sortedGuilds.filter((guild) => !guild.isPlaying);
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col pt-4 px-2 flex-1 overflow-auto gap-1">
-        {guilds.map((guild) => (
+        {playingGuilds.length > 0 && playingGuilds.map((guild) => <GuildEntry key={guild.id} guild={guild} />)}
+        {playingGuilds.length > 0 && <hr className="border-t-soft-white" />}
+        {notPlayingGuilds.map((guild) => (
           <GuildEntry key={guild.id} guild={guild} />
         ))}
       </div>
