fb894b121fb483cb7e50c32d163ff4442e5558e1
simonlary <simonlary@gmail.com>
2022-07-23 23:38:15 -0400

    Add some basic content to `NoGuildPage`.

--- dashboard/src/components/guilds/Guild/NoGuildPage.tsx
+++ dashboard/src/components/guilds/Guild/NoGuildPage.tsx
@@ -1,3 +1,12 @@
+import icon from "./background-icon.svg";
+
 export function NoGuildPage() {
-  return <div className="bg-gray-3 h-full w-full rounded-tl-md" />;
+  return (
+    <div className="flex items-center justify-center bg-gray-3 h-full w-full rounded-tl-md">
+      <div className="flex-col w-96">
+        <img className="w-full" src={icon} />
+        <div className="w-full text-center text-soft-white">Select one of your servers in the side bar to start.</div>
+      </div>
+    </div>
+  );
 }
