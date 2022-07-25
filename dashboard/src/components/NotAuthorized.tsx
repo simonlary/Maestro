40271e1d02a04afc74cb724d44ba407a83fdb4eb
simonlary <simonlary@gmail.com>
2022-07-24 23:16:20 -0400

    Improve 403 page.

--- dashboard/src/components/NotAuthorized.tsx
+++ dashboard/src/components/NotAuthorized.tsx
@@ -1,3 +1,13 @@
 export function NotAuthorized() {
-  return <div>403</div>;
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="flex-col text-center font-mono">
+        <div className="text-3xl">🚫 Not Authorized 🚫</div>
+        <div className="text-lg mt-4 mb-12">
+          Sorry, but you don&apos;t have access to the page you&apos;re trying to view.
+        </div>
+        <div className="text-9xl">403</div>
+      </div>
+    </div>
+  );
 }
--- dashboard/src/components/NotFound.tsx
+++ dashboard/src/components/NotFound.tsx
@@ -2,7 +2,7 @@ export function NotFound() {
   return (
     <div className="flex items-center justify-center h-full">
       <div className="flex-col text-center font-mono">
-        <div className="text-3xl">Not found :(</div>
+        <div className="text-3xl">🤷‍♂️ Not found 🤷‍♂️</div>
         <div className="text-lg mt-4 mb-12">Sorry, but the page you were trying to view does not exist.</div>
         <div className="text-9xl">404</div>
       </div>
