22d71a9b713360ee8a03aff8668eaf072f9a9153
simonlary <simonlary@gmail.com>
2022-07-24 23:07:57 -0400

    improve 404 page.

--- dashboard/src/components/NotFound.tsx
+++ dashboard/src/components/NotFound.tsx
@@ -1,3 +1,11 @@
 export function NotFound() {
-  return <div>404</div>;
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="flex-col text-center font-mono">
+        <div className="text-3xl">Not found :(</div>
+        <div className="text-lg mt-4 mb-12">Sorry, but the page you were trying to view does not exist.</div>
+        <div className="text-9xl">404</div>
+      </div>
+    </div>
+  );
 }
